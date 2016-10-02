7ad1433426773e04e6d539a5a200e9d714706720
grudges <pedrompsrodrigues@gmail.com>
2016-10-02 21:35:46 +0100

    dotenvgen.php;

---
+++ config/dotenvgen.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'rules' => [
+        // Ignores all files inside `vendor` except for those in `vendor/laravel`
+        'vendor' => ['laravel'],
+
+        // Ignores the `database/seeds` directory
+        'database/seeds' => [],
+    ],
+];
